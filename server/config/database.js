d47a17ad1485fbc128c7572a817f80e302a911de
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-08-14 17:41:34 +0530

    Small bug fix in withdrawl page

--- server/config/database.js
+++ server/config/database.js
@@ -1,56 +1,61 @@
 
-// if (process.env.NODE_ENV === 'production') {
-  
-//     module.exports = ({ env }) => ({
-//       connection: {
-//         client: 'mysql',
-//         connection: {
-//           host: "217.21.95.154",
-//           port: 3306,
-//           database: "u962252833_compassion_db",
-//           user: "u962252833_compassion_usr",
-//           password: ":7Nw=Z$/4^",
-//           ssl: {
-//             rejectUnauthorized: false
-//           },
-//         },
-//         debug: false,
-//       },
-//     })
-// }
-// else {
+if (process.env.NODE_ENV === 'production') {
 
-//   module.exports = ({ env }) => ({
-//     connection: {
-//       client: 'postgres',
-//       connection: {
-//         host: "127.0.0.1",
-//         port: 5432,
-//         database: "compassion_db",
-//         user: "postgres",
-//         password: "root",
-//         ssl: {
-//           rejectUnauthorized: false
-//         },
-//       },
-//       debug: false,
-//     },
-//   })
-// }
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+  module.exports = ({ env }) => ({
     connection: {
-      host: "153.92.211.54",
-      port: 5432,
-      database: "compassion_db",
-      user: "postgres",
-      password: "root",
-      ssl: {
-        rejectUnauthorized: false
+      client: 'mysql',
+      connection: {
+        // host: "217.21.95.154",
+        "host": '153.92.211.54',
+        // port: 3306,
+        port: 5432,
+        // database: "u962252833_compassion_db",
+        // user: "u962252833_compassion_usr",
+        // password: ":7Nw=Z$/4^",
+        database: "compassion_db",
+        user: "postgres",
+        password: "root",
+        ssl: {
+          rejectUnauthorized: false
+        },
       },
+      debug: false,
     },
-    debug: false,
-  },
-})
+  })
+}
+else {
+
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: "127.0.0.1",
+        port: 5432,
+        database: "compassion_db",
+        user: "postgres",
+        password: "root",
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
+    },
+  })
+}
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: "153.92.211.54",
+//       port: 5432,
+//       database: "compassion_db",
+//       user: "postgres",
+//       password: "root",
+//       ssl: {
+//         rejectUnauthorized: false
+//       },
+//     },
+//     debug: false,
+//   },
+// })
